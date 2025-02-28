483b7570a2176ff152b01b610678c3d66938e214
Njire Njeri <njirenjeri@gmail.com>
2025-02-28 18:16:27 +0300

    setter and getter methods added

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,42 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self._title = title
+        self._page_count = page_count
+        # self._turn_page = turn_page
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        self._title = title
+
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    # @property
+    # def turn_page(self):
+    #     return self._turn_page
+    
+    # @turn_page.setter
+    # def turn_page(self, turn_page):
+    #     # if isinstance(turn_page, str):
+    #     self._turn_page = turn_page
+    #     print("Flipping the page...wow, you read fast!")
+    
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self._brand = brand
+        self._size = size
+        self.condition = 'Old'
+        
+    @property
+    def brand(self):
+        return self._brand
+    
+    @brand.setter
+    def brand(self, brand):
+        if isinstance(brand, str):
+            self._brand = brand
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print('Your shoe is as good as new!')
+        self.condition = 'New'
+        # condition = 'New'
